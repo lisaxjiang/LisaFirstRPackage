636c35611f644760e5ef388f2c018a41f815fa2b
Lisa Jiang <lisajiang221@gmail.com>
2020-12-15 14:12:40 -0800

    Update test

--- tests/testthat/test-my_knn_cv.R
+++ tests/testthat/test-my_knn_cv.R
@@ -16,11 +16,3 @@ test_that("non-list train input throws error", {
 test_that("non-list cl input throws error", {
   expect_error(my_knn_cv(penguins, "spring", 5, 1))
 })
-
-test_that("non-valid k_nn throws error", {
-  expect_error(my_knn_cv(penguins, my_penguins$species, 5, 0))
-})
-
-test_that("non-valid k_cv throws error", {
-  expect_error(my_knn_cv(penguins, my_penguins$species, 0, 1))
-})
