707852677d145f0dfdb8a2ebc43eed67c1a2a97e
Lisa Jiang <lisajiang221@gmail.com>
2020-12-14 16:01:28 -0800

    Update test

---
+++ tests/testthat/test-my_t.test.R
@@ -0,0 +1,24 @@
+# within test-f_to_c.R
+test_that("my_t.test alternative is less properly", {
+  expect_is(my_t.test(my_gapminder[[4]], "less", 50), "list")
+})
+
+test_that("my_t.test alternative is greater properly", {
+  expect_is(my_t.test(my_gapminder[[4]], "greater", 50), "list")
+})
+
+test_that("my_t.test alternative is two.sided properly", {
+  expect_is(my_t.test(my_gapminder[[4]], "two.sided", 50), "list")
+})
+
+test_that("Data input throws error", {
+  expect_error(my_t.test("data", "less", 50), "list")
+})
+
+test_that("String input throws error", {
+  expect_error(my_t.test(my_gapminder[[4]], 1, 50), "list")
+})
+
+test_that("Numeric input throws error", {
+  expect_error(my_t.test(my_gapminder[[4]], "less", "mu"), "list")
+})
