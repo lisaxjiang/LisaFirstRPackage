cef0481a2d86ede71b765a3ffa8e73c464ef76e2
Lisa Jiang <lisajiang221@gmail.com>
2020-12-14 22:03:14 -0800

    Update function

--- tests/testthat/test-my_knn_cv.R
+++ tests/testthat/test-my_knn_cv.R
@@ -1,10 +1,4 @@
 #within test-my_knn_cv.R
-penguins <- data.frame("species" = my_penguins[[1]],
-                       "bill_length_mm" = my_penguins[[3]],
-                       "bill_depth_mm" = my_penguins[[4]],
-                       "flipper_length_mm" = my_penguins[[5]],
-                       "body_mass_g" = my_penguins[[6]])
-
 test_that("my_knn_cv works", {
   expect_is(my_knn_cv(penguins, penguins$species, 1, 5), "list")
 })
