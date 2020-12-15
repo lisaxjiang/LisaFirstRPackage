f2fb8926bbf3eb53614d97664c26a4cc1bbb73b9
Lisa Jiang <lisajiang221@gmail.com>
2020-12-15 08:32:47 -0800

    Updated function and test

--- R/my_knn_cv.R
+++ R/my_knn_cv.R
@@ -2,9 +2,9 @@
 #'
 #' This function performs a k-nearest neighbors cross-validation.
 #' @param train A given data frame.
-#' @param cl True value of the training data \code{train}.
-#' @param k_nn An integer indicating the number of neighbors.
+#' @param cl True values of the training data \code{train}.
 #' @param k_cv An integer indicating the number of folds.
+#' @param k_nn An integer indicating the number of neighbors.
 #' @keywords prediction
 #'
 #' @return A list containing (1) a set of the predicted variable for all the
@@ -12,13 +12,13 @@
 #'   misclassification error.
 #'
 #' @examples
-#' my_knn_cv(penguins, my_penguins$species, 1, 5)
-#' my_knn_cv(penguins, my_penguins$species, 10, 5)
+#' my_knn_cv(penguins, my_penguins$species, 5, 1)
+#' my_knn_cv(penguins, my_penguins$species, 5, 10)
 #'
 #' @export
 #' @import class magrittr randomForest
-my_knn_cv <- function(train, cl, k_nn, k_cv) {
-  # use knn() to find predicted values
+my_knn_cv <- function(train, cl, k_cv, k_nn) {
+  # find predicted values using full data
   predicted <- knn(train = train,
                    cl = cl,
                    test = train,
@@ -32,24 +32,23 @@ my_knn_cv <- function(train, cl, k_nn, k_cv) {
 
   # loop through all folds
   for(i in 1:k_cv) {
-    # get x_i and x_i*
+    # get train and test data in train
     data_train <- train[inds != i,]
     data_test <- train[inds == i,]
     # train models
     cl_train <- cl[inds != i]
     cl_test <- cl[inds == i]
-    # use knn() to find predicted values in each fold
+    # find predicted values in each fold
     fold_knn <- knn(train = data_train,
-                      cl = cl_train,
-                      test = data_test,
-                      k = k_nn)
+                    cl = cl_train,
+                    test = data_test,
+                    k = k_nn)
     # compare outputs with cl_test and calculate cv misclassification errors
     cross_val[i] <- sum(fold_knn == cl_test) / length(cl_test)
   }
   # calculate the average misclassification rate
   cv_error <- mean(cross_val)
 
-  # create a list of the predicted variables and the average cv
   # misclassification rate
   my_list <- list("class" = predicted,
                  "cv_error" = cv_error)
--- tests/testthat/test-my_knn_cv.R
+++ tests/testthat/test-my_knn_cv.R
@@ -5,17 +5,25 @@ penguins <- data.frame("bill_length_mm" = my_penguins[[3]],
                        "body_mass_g" = my_penguins[[6]])
 
 test_that("my_knn_cv works", {
-  expect_is(my_knn_cv(penguins, penguins$species, 1, 5), "list")
+  expect_is(my_knn_cv(penguins, penguins$species, 5, 1), "list")
 })
 
 test_that("my_knn_cv works", {
-  expect_is(my_knn_cv(penguins, penguins$species, 10, 5), "list")
+  expect_is(my_knn_cv(penguins, penguins$species, 5, 10), "list")
 })
 
 test_that("non-list train input throws error", {
-  expect_error(my_knn_cv("string", penguins$species, 1, 5))
+  expect_error(my_knn_cv("string", penguins$species, 1, 1))
 })
 
 test_that("non-list cl input throws error", {
-  expect_error(my_knn_cv(penguins, "spring", 1, 5))
+  expect_error(my_knn_cv(penguins, "spring", 1, 1))
+})
+
+test_that("non-valid k_nn throws error", {
+  expect_is(my_knn_cv(penguins, penguins$species, 5, 0))
+})
+
+test_that("non-valid k_cv throws error", {
+  expect_is(my_knn_cv(penguins, penguins$species, 0, 1))
 })
